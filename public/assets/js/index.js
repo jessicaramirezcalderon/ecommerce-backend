dfd5f4d53b45c3d22c559b98c037c06086460c88
Jessi RC <jessy.ramirez23@gmail.com>
2021-03-21 16:23:39 -0400

    added handlebar templates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,3 +7,66 @@
 // * Your app will store every burger in a database, whether devoured or not.
 
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+$(function() {
+    $(".delete-button").on("click", function(event) {
+      console.log("Delete worked");
+      console.log(this);
+      const button = $(this);
+      // grabs id from button
+      const id = button.attr("data-id");
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function(results) {
+          console.log(results);
+          location.reload();
+        }
+      );
+    });
+    $(".change-status").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newsleep");
+  
+      var newSleepState = {
+        sleepy: newSleep
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newSleepState
+      }).then(
+        function() {
+          console.log("changed status to", newSleep);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      const newCat = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
