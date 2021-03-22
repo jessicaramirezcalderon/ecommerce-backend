2ef7651e36546349b30fcab1566d52b86417d75b
Jessi RC <jessy.ramirez23@gmail.com>
2021-03-21 21:20:08 -0400

    Updated creds.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 // Require mysql
 const mysql = require("mysql");
 
+const isProduction = process.env.NODE_ENV === 'production';
 // Set up our connection information
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_URL ? 'us-cdbr-east-03.cleardb.com' : "localhost",
+  host: isProduction ? 'us-cdbr-east-03.cleardb.com' : "localhost",
   port: 3306,
-  user: process.env.DATABASE_URL ? "b157f750519cb3" : "root",
-  password: process.env.DATABASE_URL ? 'ec38ff26' : "cocococo",
-  database: process.env.DATABASE_URL ? 'heroku_5bdfacff12e40f0' : "burgers_db"
+  user: isProduction ? "b157f750519cb3" : "root",
+  password: isProduction ? 'ec38ff26' : "cocococo",
+  database: isProduction ? 'heroku_5bdfacff12e40f0' : "burgers_db"
 });
 
 // Connect to the database
